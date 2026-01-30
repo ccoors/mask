09c306e68fc87075865f0123ca303bef4553725d
Christian Friedrich Coors <me@ccoors.de>
2026-01-30 22:13:57 +0100

    fix: stuff

--- scenes/globals.gd
+++ scenes/globals.gd
@@ -2,15 +2,21 @@ extends Node
 
 const MASKS = [
     {
-        "name": "Foo Mask",
+        "name": "Lava mask",
         "input_action": "mask_1",
-        "tint_color": Color(0.938, 0.0, 0.62, 0.5),
+        "tint_color": Color(0.937, 0.25, 0.0, 0.5),
         "collision_mask": 2,
     },
     {
-        "name": "Bar Mask",
+        "name": "Wood mask",
         "input_action": "mask_2",
         "tint_color": Color(0.0, 0.65, 0.025, 0.5),
         "collision_mask": 3,
+    },
+    {
+        "name": "Water mask",
+        "input_action": "mask_3",
+        "tint_color": Color(0.0, 0.589, 0.753, 0.5),
+        "collision_mask": 4,
     },
 ]
