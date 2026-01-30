1cb31d4a839eb9eee7a00cdccd4d120bdb3b13bf
Christian Friedrich Coors <me@ccoors.de>
2026-01-30 21:26:30 +0100

    feat: level with custom collision masks

--- scenes/player.gd
+++ scenes/player.gd
@@ -11,21 +11,34 @@ const MASKS = [
         "name": "Foo Mask",
         "input_action": "mask_1",
         "tint_color": Color(0.938, 0.0, 0.62, 0.5),
+        "collision_mask": 2,
     },
     {
         "name": "Bar Mask",
         "input_action": "mask_2",
         "tint_color": Color(0.0, 0.65, 0.025, 0.5),
+        "collision_mask": 3,
     },
 ]
 
 func _ready() -> void:
-    mask_changed.emit(MASKS[0])
+    set_mask(0)
+    
+func set_mask(idx: int) -> void:
+    var mask = MASKS[idx]
+    mask_changed.emit(MASKS[idx])
+    var set_mask = 0
+    print("--")
+    for i in range(5):
+        if mask["collision_mask"] != i:
+            print("Set bit ", i)
+            set_mask |= (1 << i)
+    collision_mask = set_mask
 
 func _process(delta: float) -> void:
-    for mask in MASKS:
-        if Input.is_action_just_pressed(mask["input_action"]):
-            mask_changed.emit(mask)
+    for n in range(MASKS.size()):
+        if Input.is_action_just_pressed(MASKS[n]["input_action"]):
+            set_mask(n)
 
 func _physics_process(delta: float) -> void:
     var direction := Input.get_vector("left", "right", "up", "down")
