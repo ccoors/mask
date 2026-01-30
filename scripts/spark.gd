88403406feb597182dcb794c74cceeae621dea6d
Imke Schrigten <i.schrigten@web.de>
2026-01-30 22:17:36 +0100

    feat: base particles

---
+++ scripts/spark.gd
@@ -0,0 +1,18 @@
+extends GPUParticles2D
+
+@export var enabled := true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	emitting = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func set_enabled(isEnabled: bool):
+	enabled = isEnabled
+	emitting = isEnabled
+	
