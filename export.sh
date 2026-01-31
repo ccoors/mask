c18b96ce51e7f3b8f185bb7dad867321db2d1ed3
Christian Friedrich Coors <me@ccoors.de>
2026-01-31 13:33:02 +0100

    fix: export script

--- export.sh
+++ export.sh
@@ -2,12 +2,13 @@
 
 set -euxo pipefail
 
-rm -rfv export/
+DEST_FILE="elektrisches_maskenspiel.zip"
+
+rm -rfv export/ ${DEST_FILE}
 mkdir -p export/{linux,windows}
 
 godot -v --headless --export-release "Linux" ./export/linux/elektrisches_maskenspiel.x86_64
 godot -v --headless --export-release "Windows Desktop" ./export/windows/elektrisches_maskenspiel.exe
 
 cd export
-zip -9 -r linux.zip linux
-zip -9 -r windows.zip windows
+zip -4 -r ../${DEST_FILE} .
