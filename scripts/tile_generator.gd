0e5a45633e6b3917cb1f899f869ad0e308d3dcdf
Andreas Bresser <self@andreasbresser.de>
2026-01-31 11:35:54 +0100

    update tiles

--- scripts/tile_generator.gd
+++ scripts/tile_generator.gd
@@ -1,35 +1,39 @@
 @tool
 extends TileMapLayer
 
+class_name TileGenerator
+
+@export_tool_button("Recreate Map") var execute_action = gen_map
+
+@export var seed: int = 0:
+    set(value):
+        seed = value
+        gen_map()
+
+@export_tool_button("Randomize seed", "RandomNumberGenerator") var randomize_seed_action = randomize_seed
+
+func randomize_seed():
+    seed = randi()
+
 @export var size: int = 128:
     set(value):
         if value > 0:
             gen_map()
             size = value
 
-@export var recreate_map: bool = false:
-    set(value):
-        if value:
-            gen_map()
-            recreate_map = false
-
-var centerSquareSideLength: int = 10 # in tiles
-var wallThickness: int = 100 # in tiles
-
-
 func gen_map() -> void:
     clear()
     var noise = FastNoiseLite.new()
-    noise.seed = randi() % 10000 + 1
-    noise.noise_type = FastNoiseLite.TYPE_VALUE
-    for y in range(0, size):
-        for x in range(0, size):
+    noise.seed = seed
+    noise.noise_type = FastNoiseLite.TYPE_PERLIN
+    noise.frequency = 0.02
+    noise.fractal_lacunarity = 2
+    noise.fractal_octaves = 2
+    var halfsize = ceil(size/2)
+    for y in range(-halfsize, halfsize):
+        for x in range(-halfsize, halfsize):
             var val = noise.get_noise_2d(x, y)
-            var tileId = round(abs(val) * 3);
-            set_cell(Vector2i(x, y), tileId, Vector2i.ZERO)
-
-func fillCenterSquare(sideLength: int, tileId: int) -> void:
-    var halfSideLength: float = 0.5 * sideLength
-    for y in range(ceil(-halfSideLength), ceil(halfSideLength)):
-        for x in range(ceil(-halfSideLength), ceil(halfSideLength)):
+            var tileId = round(abs(val) * 5);
+            if tileId > 2:
+                tileId = 2
             set_cell(Vector2i(x, y), tileId, Vector2i.ZERO)
